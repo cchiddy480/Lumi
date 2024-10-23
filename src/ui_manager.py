4c09feeb941ef31d192fd30d281f3ba696c6d17a
cchiddy480 <71733645+cchiddy480@users.noreply.github.com>
2024-10-23 13:29:19 +0100

    Update ui_manager.py
    Creating skeleton class for UIManager

--- src/ui_manager.py
+++ src/ui_manager.py
@@ -1,1 +1,9 @@
+class UIManager:
+    def __init__(self):
+        # Initialize the list to store UI components
+        self.components = []
+
+    def add_component(self, component):
+        # Add a new component to the UI manager
+        self.components.append(component)
 
